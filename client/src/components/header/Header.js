b74bf056304d1f3b4922a5b37cf7096199d697d5
TongguangZhang <tonggz250@gmail.com>
2023-10-15 11:41:26 -0400

    added social icons

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,4 +1,7 @@
 import React, { useEffect } from "react"
+import github_icon from "../../assets/github.png"
+import linkedin_icon from "../../assets/linkedin.png"
+import email_icon from "../../assets/email.png"
 import "../../styles/header.scss"
 
 const Header = () => {
@@ -6,7 +9,6 @@ const Header = () => {
         const start_date = new Date("2022-09-05")
         const end_date = new Date("2027-04-30")
         const current_date = new Date()
-        // const rem = 10.5
         const progress = (current_date - start_date) / (end_date - start_date)
         return progress
     }
@@ -23,9 +25,19 @@ const Header = () => {
                 <span id="lines_thin"></span>
             </div>
             <h2>Engineering Student</h2>
+            <div className="link-icons">
+                <a href="https://github.com/TongguangZhang" target="_blank">
+                    <img className="link-icon" src={github_icon} alt="github"></img>
+                </a>
+                <a href="https://www.linkedin.com/in/tongzh/" target="_blank">
+                    <img className="link-icon" src={linkedin_icon} alt="linkedin"></img>
+                </a>
+                <a href="mailto:tong.zhang@uwaterloo.ca" target="_blank">
+                    <img className="link-icon" src={email_icon} alt="email"></img>
+                </a>
+            </div>
         </div>
     )
 }
 
 export default Header
-
